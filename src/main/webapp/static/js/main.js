3d73f3fce73168cef59b9ec34565087dc2cbc130
Tony <info@toninocatapano.com>
2015-03-23 23:20:38 +0100

    beautified ;)

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,168 +1,165 @@
-var playField;
-var openBoxes;
-var size;
-var nrMines;
-var nrBoxes;
-var nrGuessed;
+var field; //matrix of int representing the game board
+var openBoxes; //number of open boxes
+var size; //size of the game board
+var nrMines; //nr of Mines present on the game board
+var nrBoxes; // nr of the boxes still to check
+var nrGuessed; //nr of the guessed boxes
 
-function openBox(i, j) {  
-    if (playField[i][j] < 0){
-    	lose(i,j);
-    }
-    else if (playField[i][j] > 0 && openBoxes[i][j] === 0){
-    	reveal(i,j);
-    }else{
-    	openRec(i,j);    	
-    }
-}
+// WIN = nrMines == nrBoxes == nrGuessed
 
+$(document).ready(function() {
+	init();
+	$(this).on("contextmenu", ".gamebuttons", function(e) {
+		var me = $(this);
+		var i = parseInt(me.attr("i"));
+		var j = parseInt(me.attr("j"));
+		changeColor(i, j);
+		return false;
+	});
 
+	$("#new").click(function() {
+		$.ajax({
+			url : "/minefield/game/new.do",
+			data : null,
+			type : "GET",
+			async : false
+		}).done(function() {
+			init();
+		});
+	});
+});
 
-
-
-
-
-
-function openRec(i, j){
-	if(i>=size || i<0 || j>=size || j<0 || playField[i][j] < 0 || openBoxes[i][j] !== 0) return;
-
-
-	if (playField[i][j] > 0){
-		reveal(i,j);
-	}else{
-		hide(i,j);
-		openRec(i-1, j);
-		openRec(i+1, j);
-		openRec(i, j-1);
-		openRec(i, j+1);
+function openBox(i, j) {
+	if (field[i][j] < 0) {
+		lose(i, j);
+	} else if (field[i][j] > 0 && openBoxes[i][j] === 0) {
+		reveal(i, j);
+	} else {
+		openRec(i, j);
 	}
-	
 }
 
+function openRec(i, j) {
+	if (i >= size || i < 0 || j >= size || j < 0 || field[i][j] < 0
+			|| openBoxes[i][j] !== 0)
+		return;
+
+	if (field[i][j] > 0) {
+		reveal(i, j);
+	} else {
+		hide(i, j);
+		openRec(i - 1, j);
+		openRec(i + 1, j);
+		openRec(i, j - 1);
+		openRec(i, j + 1);
+	}
+}
+
+function initOpenBoxes() {
+	openBoxes = new Array(size);
+	for (var i = 0; i < size; i++) {
+		openBoxes[i] = new Array(size);
+		for (var j = 0; j < size; j++) {
+			openBoxes[i][j] = 0
+		}
+	}
+}
+
+function redrawButtons(container) {
+	$(container).html("");
+	for (var i = 0; i < size; i++) {
+		for (var j = 0; j < size; j++) {
+			$('.container').append(
+					'<input class="gamebuttons" style="height:25px; width:25px" i="'
+							+ i + '" j="' + j
+							+ '" type="button" value="&nbsp;"/>');
+		}
+		$('.container').append('<br/>');
+	}
+
+	$(".gamebuttons").click(function(event) {
+		var me = $(this);
+		var i = parseInt(me.attr("i"));
+		var j = parseInt(me.attr("j"));
+		openBox(i, j);
+	});
+}
 
 function init() {
 	nrGuessed = 0;
-	
-    $.ajax({
-        url: "/minefield/game/getSize.do",
-        data: null,
-        type: "GET",
-        async: true
-    }).done(function(data) {
-        size = data;     
-        nrBoxes = size * size;
-        
-        openBoxes = new Array(size);
-        for (var i = 0; i < size; i++) {
-        	openBoxes[i] = new Array(size);
-        }
-        for(var k= 0; k<size; k++){
-        	for (var l=0; l<size; l++){
-        		openBoxes[k][l] = 0;
-        	}
-        }
-        
 
-	    $('.container').html("");
-	    for (var i = 0; i < size; i++) {
-	        for (var j = 0; j < size; j++) {
-	            $('.container').append('<input class="gamebuttons" style="height:25px; width:25px" i="' + i + '" j="' + j + '" type="button" value="&nbsp;"/>');
-	        }
-	        $('.container').append('<br/>');
-	    }    
-    });
-	
-	
-    $.ajax({
-        url: "/minefield/game/getField.do",
-        data: null,
-        type: "GET",
-        async: true
-    }).done(function(data) {
-        playField = data;
-	    $(".gamebuttons").click(function(event) {
-	        var me = $(this);
-	        var i = parseInt(me.attr("i"));
-	        var j = parseInt(me.attr("j"));
-	        openBox(i, j);
-	    });		        
-    });
-    
-    
-    $.ajax({
-        url: "/minefield/game/getNrMines.do",
-        data: null,
-        type: "GET",
-        async: true
-    }).done(function(data) {
-        nrMines = data;
-    });
-    
+	$.ajax({
+		url : "/minefield/game/getSize.do",
+		data : null,
+		type : "GET",
+		async : true
+	}).done(function(data) {
+		size = data;
+		nrBoxes = size * size;
+		initOpenBoxes();
+		redrawButtons('.container');
+	});
+
+	$.ajax({
+		url : "/minefield/game/getField.do",
+		data : null,
+		type : "GET",
+		async : true
+	}).done(function(data) {
+		field = data;
+	});
+
+	$.ajax({
+		url : "/minefield/game/getNrMines.do",
+		data : null,
+		type : "GET",
+		async : true
+	}).done(function(data) {
+		nrMines = data;
+	});
+
 }
 
-function hide(i,j){
+function hide(i, j) {
 	openBoxes[i][j] = -1;
-	$('input[i="' + i + '"][j="' + j + '"]').css("opacity", "0").attr("disabled", "disabled");
+	$('input[i="' + i + '"][j="' + j + '"]').css("opacity", "0").attr(
+			"disabled", "disabled");
 	nrBoxes--;
 	checkWin();
 }
 
-function reveal(i,j){
+function reveal(i, j) {
 	openBoxes[i][j] = -1;
-	$('input[i="' + i + '"][j="' + j + '"]').val(playField[i][j]);
+	$('input[i="' + i + '"][j="' + j + '"]').val(field[i][j]);
 	nrBoxes--;
 	checkWin();
 }
 
-function lose(i,j){
+function lose(i, j) {
 	alert("you lose!");
-	//openBoxes[i][j] = -1;
 	$('input[i="' + i + '"][j="' + j + '"]').val('X');
-	$('.gamebuttons').attr("disabled","disabled");
-	//nrMines--;
+	$('.gamebuttons').attr("disabled", "disabled");
 }
 
-function changeColor(i,j){
-	if(openBoxes[i][j] < 0){
+function changeColor(i, j) {
+	if (openBoxes[i][j] < 0) {
 		return;
-	}else if(openBoxes[i][j] === 0){
+	} else if (openBoxes[i][j] === 0) {
 		openBoxes[i][j] = 1;
-		$('input[i="' + i + '"][j="' + j + '"]').css("background-color","black");
+		$('input[i="' + i + '"][j="' + j + '"]').css("background-color",
+				"black");
 		nrGuessed++;
-	}else if(openBoxes[i][j] > 0){
+	} else if (openBoxes[i][j] > 0) {
 		openBoxes[i][j] = 0;
-		$('input[i="' + i + '"][j="' + j + '"]').css("background-color","");
+		$('input[i="' + i + '"][j="' + j + '"]').css("background-color", "");
 		nrGuessed--;
 	}
 	checkWin();
-		
 }
 
-function checkWin(){
-	if(nrMines === nrBoxes && nrMines === nrGuessed) alert("you win!");
+function checkWin() {
+	if (nrMines === nrBoxes && nrMines === nrGuessed){
+		alert("you win!");
+		$('.gamebuttons').attr("disabled", "disabled");
+	}
 }
-
-$(document).ready(function() {		
-
-	
-	
-	$(this).on("contextmenu", ".gamebuttons", function(e){
-		var me = $(this);
-        var i = parseInt(me.attr("i"));
-        var j = parseInt(me.attr("j"));
-	   changeColor(i,j);
-	   return false;
-	});
-	 
-    $("#new").click(function() {
-        $.ajax({
-            url: "/minefield/game/new.do",
-            data: null,
-            type: "GET",
-            async: false
-        }).done(function(){
-	        init();
-	    });
-    });		
-    init();
-});
